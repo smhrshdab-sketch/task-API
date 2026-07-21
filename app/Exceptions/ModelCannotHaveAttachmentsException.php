d2ae49758e55dcdbfebe7f6e4445ec97932e9dc6
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-21 14:52:32 +0330

    create new exception for stop attaching file by removed user

--- app/Exceptions/ModelCannotHaveAttachmentsException.php
+++ app/Exceptions/ModelCannotHaveAttachmentsException.php
@@ -1,2 +1,27 @@
 <?php
-    class ModelCannotHaveAttachmentsException extends \Exception {}
+    namespace App\Exceptions;
+
+use App\Models\AuditLog;
+use Exception;
+use Illuminate\Http\JsonResponse;
+    
+    class ModelCannotHaveAttachmentsException extends Exception{
+        public function report(): void{
+            AuditLog::create([
+                'subject_type' => 'Attachment',
+                'causer_id'    => current_membership()?->id,
+                'event'        => 'error',
+                'description'  => $this->getMessage(),
+                'url'          => request()->fullUrl(),
+                'method'       => request()->method(),
+                'metadata'     => json_encode(['exception' => get_class($this)]),
+            ]);
+        }
+        public function render($request): JsonResponse{
+            return response()->json([
+                'success' => false,
+                'message' => 'This entity can not attach files',
+            ], 403);
+        }
+    }
+    
