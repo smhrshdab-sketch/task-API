1c5966a9f74df88478a117c2f446061ed9487c07
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-21 15:14:06 +0330

    Modify and add some logsn to find bugs

--- app/Http/Requests/StoreTaskAttachmentRequest.php
+++ app/Http/Requests/StoreTaskAttachmentRequest.php
@@ -3,14 +3,17 @@
 namespace App\Http\Requests;
 
 use App\Contracts\Attachable;
+use App\Http\Requests\BaseAttachmentRequest;
 use App\Services\AttachmentAuthorizationService;
+use Illuminate\Support\Facades\Log;
 
 class StoreTaskAttachmentRequest extends BaseAttachmentRequest
 {
     protected function getAttachableModel(): ?Attachable{
         // Get the task from the route and return it
+        Log::info('👿 StoreTaskAttachmentRequest getAttachableModel REACHED 👿');
         $task = $this->route('task');
-        logger('route tassk [getAttachableModel]: ',[$task]);
+        logger('Selected task is: ',[$task->title]);
         // Make sure it implements Attachable
         if ($task instanceof Attachable) {
             return $task;
@@ -21,15 +24,24 @@ class StoreTaskAttachmentRequest extends BaseAttachmentRequest
     
     protected function authorizeAttachment(Attachable $attachable): bool
     {
+        Log::info('🤖 StoreTaskAttachmentRequest authorizeAttachment REACHED 🤖');
         $task = $attachable;
         $membership = current_membership();
-        logger('route tassk [authorizeAttachment]: (task,membership) ',[$task,$membership]);
+        logger('route tassk [authorizeAttachment]: ',[$task->title]);
         if (!$membership) {
             return false;
         }
-        // Check if user is in the same department as the task
-        $auth = app(AttachmentAuthorizationService::class);
-        logger('route tassk [authorizeAttachment]: (task,membership,auth) ',[$task,$membership,$auth]);
-        return $auth->canAttach($task->department);
+        
+        // Check if user is in the same department as the task    
+        if($membership->department->id !== $task->department->id){
+            Log::info('Now we know user and task are NOT in same department'); 
+            logger('department->id: ',[$membership->department->id]);
+            Log::info('task department ID: ',[$task->department->id]);           
+            return false;
+        }
+        Log::info('Now we know user and task are in same department');
+        $result = app(AttachmentAuthorizationService::class)->canAttach($task->department);
+        Log::info('AttachmentAuthorizationService result: ',[$result]);        
+        return $result;
     }
 }
