a45e17220b7f28b24dd36c5e6d9ad7b202f33415
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-21 15:05:34 +0330

    Add some logs to find bugs

--- app/Http/Requests/BaseAttachmentRequest.php
+++ app/Http/Requests/BaseAttachmentRequest.php
@@ -19,17 +19,20 @@ abstract class BaseAttachmentRequest extends FormRequest
         $attachable = $this->getAttachableModel();
         
         if (!$attachable) {
+            logger('BaseAttachmentRequest_attachable: ',[attachable]);
             return false;
         }
         
         $membership = current_membership();
         
         if (!$membership) {
+            logger('BaseAttachmentRequest_membership: ',[$membership]);
             return false;
         }
         
         // Check if the model can have attachments
         if (!$attachable->canHaveAttachments()) {
+            logger('BaseAttachmentRequest_canHaveAttachments: ',[$attachable->canHaveAttachments()]);
             return false;
         }
         
@@ -54,18 +57,22 @@ abstract class BaseAttachmentRequest extends FormRequest
      */
     public function rules(): array
     {
-         return [
-            'attachment' => ['required', 'file', 'max:104857600', 'mimes:jpg,jpeg,png,pdf,doc,docx'],
+         $rule = [
+            'attachment' => ['required', 'file', 'max:104857600', 'mimes:jpg,jpeg,png,pdf,doc,docx,zip,mpeg,mp4,mp3,txt'],
             'disk' => ['nullable', 'string', 'in:public,s3,local'],
             'is_public' => ['sometimes', 'boolean'],
         ];
+        logger('BaseAttachmentRequest_rules: ',[$rule]);
+        return $rule;
     }
-    protected function prepareForValidation(): void
-    {
+    protected function prepareForValidation(): void{
+        logger('BaseAttachmentRequest_prepareForValidation recive');
         if ($this->has('is_public')) {
+            logger('if ');
             $this->merge([
                 'is_public' => filter_var($this->is_public, FILTER_VALIDATE_BOOLEAN)
             ]);
+            logger('after if');
         }
     }
 }
